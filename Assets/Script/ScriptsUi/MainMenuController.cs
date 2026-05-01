2d8f8ddd4935df88c2a4839ccb7ea00a1a14056f
JihunKIm <solev0025@gmail.com>
2026-05-01 22:27:12 +0900

    UI 패널 외관 기초 구현
    ui패널 구현 사항들
    
    -게임 오버 패널(사망시)
    -게임중 일시정지 패널(esc키로 진입. 임시용)
    -설정 화면 패널(닫기 버튼만 있음)
    
    MainMenuController.cs로 패널간 전환 확인.
    게임 시작 화면
    -게임시작버튼
    -설정 버튼
    -나가기 버튼
    
    게임 오버 화면
    -재시작 버튼(게임 시작시 화면으로 넘기기 처리)
    -메인메뉴 버튼(메인메뉴로 임시로 가게 설정. 추후 변경 고려)
    
    일시정지 화면
    -게임 재개 버튼
    -메인메뉴 버튼
    
    데이터 상관없이 패널 전환만 테스트해본것이므로. 추후 버튼 입력시 게임내 정보들 초기화 및 로드 추가 예정.
    설정/일시정지/게임오버 패널은 현재 프리팹화 X.

--- Assets/Script/ScriptsUi/MainMenuController.cs
+++ Assets/Script/ScriptsUi/MainMenuController.cs
@@ -1,14 +1,27 @@
+//현재는 패널 전환만 확인하는 스크립트. 인게임 내 데이터 연동은 추후 예정.
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField] private GameObject startMenuPanel;
     [SerializeField] private GameObject settingsPanel;
     [SerializeField] private GameObject gamePanel;
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private GameObject pausePanel;
+    
 
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            OnPause();
+    }
     public void OnStartGame()
     {
         startMenuPanel.SetActive(false);
+        gameOverPanel.SetActive(false);
         gamePanel.SetActive(true);
     }
 
@@ -21,6 +34,28 @@ public class MainMenuController : MonoBehaviour
     {
         settingsPanel.SetActive(false);
         startMenuPanel.SetActive(true);
+    }
+    public void OnGameRestart()
+    {
+        gameOverPanel.SetActive(false);
+        gamePanel.SetActive(true);  
+    }
+    public void OnMainMenu()
+    {
+        pausePanel.SetActive(false);
+        gameOverPanel.SetActive(false);
+        startMenuPanel.SetActive(true);
+    }
+    public void OnPause()
+    {
+        bool isPaused = pausePanel.activeSelf;
+        pausePanel.SetActive(!isPaused);
+        gamePanel.SetActive(isPaused);
+    }
+    public void OnResume()
+    {
+        pausePanel.SetActive(false);
+        gamePanel.SetActive(true);
     }
 
     public void OnQuit()
