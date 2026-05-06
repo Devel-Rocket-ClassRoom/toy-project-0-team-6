845bac93c720480f587887e15da193716d584b9f
조강현 <jokh980324@naver.com>
2026-05-06 14:20:46 +0900

    중간 저장

---
+++ Assets/Script/ScriptsUi/KeyConfigRow.cs
@@ -0,0 +1,111 @@
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class KeyConfigRow : MonoBehaviour
+{
+    public string key;
+    public string subKey;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI keyButtonText;
+
+    public Button assignButton;
+    public Button resetButton;
+
+    public string type = "Keyboard";
+    public InputActionAsset asset;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        nameText.text = subKey.Length > 0 ? subKey : key;
+        InputBinding bind;
+        if (subKey.Length > 0)
+        {
+            Debug.Log(subKey);
+            bind = asset.FindAction(key).bindings.First((k) => k.name.Equals(subKey.ToLower()) && k.groups.Contains("Keyboard"));
+        }
+        else
+        {
+            bind = asset.FindAction(key).bindings.First((k) => k.groups.Contains("Keyboard"));
+        }
+
+        keyButtonText.text = InputControlPath.ToHumanReadableString(bind.effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice);
+    }
+
+    public void AssignKey()
+    {
+        InputAction action = asset.FindAction(key);
+       
+        int bindIndex = -1;
+        var bindingsList = action.bindings.ToList();
+        if (!string.IsNullOrEmpty(subKey))
+        {
+            bindIndex = bindingsList.FindIndex(x => x.isPartOfComposite && x.name == subKey.ToLower());
+        }
+        else
+        {
+            bindIndex = bindingsList.FindIndex(x => !x.isPartOfComposite && x.groups != null && x.groups.Contains("Keyboard"));
+        }
+
+        Debug.Log(bindIndex);
+        action.Disable();
+        if (bindIndex == -1)
+        {
+            var rebindOperation = action.PerformInteractiveRebinding()
+                .WithControlsExcluding("<Pointer>/position")
+                .WithControlsExcluding("<Pointer>/delta")
+                .WithCancelingThrough("<Keyboard>/escape")
+                .WithTimeout(5f)
+                .OnMatchWaitForAnother(0.1f)
+                .OnComplete(op =>
+                {
+                    action.Enable();
+                    op.Dispose();
+                    Refresh();
+                })
+                .OnCancel(op =>
+                {
+                    Debug.Log("CANCELED");
+                    action.Enable();
+                    op.Dispose();
+                })
+                .Start();
+        }
+        else
+        {
+            var rebindOperation = action.PerformInteractiveRebinding(bindIndex)
+                .WithControlsExcluding("<Pointer>/position")
+                .WithControlsExcluding("<Pointer>/delta")
+                .WithCancelingThrough("<Keyboard>/escape")
+                .WithTimeout(5f)
+                .OnMatchWaitForAnother(0.1f)
+                .OnComplete(op =>
+                {
+                    action.Enable();
+                    op.Dispose();
+                    Refresh();
+                })
+                .OnCancel(op =>
+                {
+                    Debug.Log("CANCELED");
+                    action.Enable();
+
+                    op.Dispose();
+                })
+                .Start();
+        }
+    }
+
+    public void ResetKey()
+    {
+
+    }
+}
