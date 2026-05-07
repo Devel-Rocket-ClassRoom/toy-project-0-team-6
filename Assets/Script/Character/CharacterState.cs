f50b26961a7293bde25a6b624e84dac2a287d7d7
최진우 <justly2000@naver.com>
2026-05-07 09:41:54 +0900

    플레이어 공격 클릭 스테미너 계속 다는거 수정

--- Assets/Script/Character/CharacterState.cs
+++ Assets/Script/Character/CharacterState.cs
@@ -77,6 +77,8 @@ public class CharacterState : MonoBehaviour, IDamageable
     private readonly string BossTag = "Boss";
     public int attackCount = 0;
     private Coroutine vibration=null;
+    private bool canDamage = true;
+
     public int CurrentHealth
     {
         get { return currentHealth; }
@@ -210,9 +212,10 @@ public class CharacterState : MonoBehaviour, IDamageable
     {
         if (currentState == StateType.Dodge ||
             currentState == StateType.Damaged ||
-            currentState == StateType.Die)
+            currentState == StateType.Die || !canDamage)
             return;
 
+        canDamage = false;
         currentState = StateType.Attack;
         DamageVO damage = new DamageVO();
         damage.amount = Power;
@@ -344,10 +347,11 @@ public class CharacterState : MonoBehaviour, IDamageable
 
     public void DisableAttack()
     {
-        AttackZone.Attackable = false;
         if(characterMove.commandQueue.Count == 0)
         {
             characterMove.EndAttack();
+            AttackZone.Attackable = false;
+            canDamage = true;
         }
     }
 }
